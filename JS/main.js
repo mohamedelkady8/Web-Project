f9421239144b74a2c22b3299f03b5e3dac45fd28
Heba Yasser <113070904+heba516@users.noreply.github.com>
2023-11-27 02:07:58 +0200

    Update

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,6 @@ let BoxesAnimations = document.querySelectorAll('.ani');
 window.onscroll = function() {
     BoxesAnimations.forEach ((animation) => {
         let OffsetTop = animation.offsetTop;
-        let OuterHeight = animation.offsetHeight;
         let windowHeight = this.innerHeight;
         let windowScrollTop = this.scrollY;
         if (windowScrollTop > (OffsetTop - windowHeight)) {
