fdd2d04478efccadb371835c8e1b35fc01b97e41
Heba Yasser <hebay99@gmail.com>
2023-11-29 04:09:02 +0200

    flower store

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,8 @@
-//animation
+//animation & nav
+let nav = document.querySelector('nav');
 let BoxesAnimations = document.querySelectorAll('.ani');
-
 window.onscroll = function() {
+    nav.style.position = 'fixed';
     BoxesAnimations.forEach ((animation) => {
         let OffsetTop = animation.offsetTop;
         let windowHeight = this.innerHeight;
@@ -12,3 +13,34 @@ window.onscroll = function() {
     })
 }
 
+
+
+//form validation
+let form = document.querySelector("form");
+let emailInput = document.getElementById('emailInput');
+let passwordInput = document.getElementById('passwordInput');
+let messageBox = document.getElementById('messageBox');
+let messageText = document.getElementById('messageText');
+
+
+form.addEventListener(('submit'), (e)=> {
+    e.preventDefault();
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    let validEmail = /\b[A-Za-z0-9._-]+@(gmail|yahoo).com/ig;
+    let validPassword =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d@$!%*?&]){6,}/ig;
+
+    if (email.match(validEmail) && (password.match(validPassword))){
+        window.location.href = "products.html";
+    }else {
+        form.style.display = 'none';
+        messageBox.style.display = 'block';
+    }
+});
+
+
+//close msg box
+function closeMsg() {
+    form.style.display = 'block';
+    messageBox.style.display = 'none';
+}
